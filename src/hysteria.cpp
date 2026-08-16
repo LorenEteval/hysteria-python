555d6af3568891d51b8bb1c2cc0e19278a2ddcf4
Loren Eteval <loren.eteval@proton.me>
2026-08-16 16:53:10 +0800

    Add Linux and Windows ARM64 wheel builds

--- src/hysteria.cpp
+++ src/hysteria.cpp
@@ -1,9 +1,25 @@
 #include <string>
+#if defined(__MINGW32__) && defined(_M_ARM64)
+    // CPython 3.14t uses MSVC's __getReg(18) intrinsic to read the Windows
+    // ARM64 thread environment block, but LLVM-MinGW does not provide it.
+    // Windows reserves x18 for that pointer, so provide the equivalent here.
+    #include <cstdint>
+    static inline std::uintptr_t getArm64ThreadPointer()
+    {
+        std::uintptr_t value;
+        __asm__ __volatile__("mov %0, x18" : "=r"(value));
+        return value;
+    }
+    #define __getReg(registerNumber) getArm64ThreadPointer()
+#endif
 #if defined _WIN64
     #define _hypot hypot
     #include <cmath>
 #endif
 #include <pybind11/pybind11.h>
+#if defined(__MINGW32__) && defined(_M_ARM64)
+    #undef __getReg
+#endif
 
 #include "hysteria.h"
 
@@ -27,7 +43,11 @@ namespace {
         }
     }
 
-    PYBIND11_MODULE(hysteria, m, py::mod_gil_not_used()) {
+    // TODO: After auditing and testing the C++ and Go paths for free-threaded
+    // safety, use PYBIND11_MODULE(hysteria, m, py::mod_gil_not_used()) so
+    // importing this extension does not cause free-threaded CPython to enable
+    // the GIL.
+    PYBIND11_MODULE(hysteria, m) {
         m.def("startFromJSON",
             &startFromJSON,
             "Start Hysteria client with JSON, ACL rule and MMDB",
